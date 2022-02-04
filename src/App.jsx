d812b9cedc665961682d8612cf7953d3a637a895
jero237 <jeronimobalestra@gmail.com>
2022-02-04 18:43:44 -0300

    Added css animations and randomized default quote

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,44 @@ import './App.css';
 import React from 'react';
 import QuoteBox from './components/quote-box';
 import quotes from './quotes.json';
-import RandomColor from './colors.js'
+import RandomColor from './randomColor.js'
 
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      quote: "Be yourself; everyone else is already taken.",
-      author: "Oscar Wilde",
-      color: "red"
+      quote: "",
+      author: "",
+      color: ""
     }
 
     this.randomQuote = this.randomQuote.bind(this)
-
   }
 
   randomQuote(){
+    do {
+      var color = RandomColor();
+    } while (color == this.state.color);
+
+    let number = Math.floor(Math.random()*quotes.length)
+    
     this.setState({
-      quote: quotes[Math.floor(Math.random()*quotes.length)].quote,
-      author: quotes[Math.floor(Math.random()*quotes.length)].author,
-      color: RandomColor()
+      quote: quotes[number].quote,
+      author: quotes[number].author,
+      color: color
     })
+  }
+
+  componentDidMount(){
+    this.randomQuote()
   }
 
   render() {
     return (
-      <div className="App" style={{backgroundColor: this.state.color}}>
+      <div className="App" style={{backgroundColor: this.state.color, transition: "1s"}}>
         <QuoteBox quote={this.state.quote} author={this.state.author} color={this.state.color} randomQuote={this.randomQuote}/>
+        <a href="https://github.com/jero237" id='github' target="_blank"><i class="fab fa-github"></i> jero237</a>
       </div>
     );
   }
--- src/components/quote-box.jsx
+++ src/components/quote-box.jsx
@@ -1,11 +1,11 @@
 const QuoteBox = (props) => {
     return (
         <div id="quote-box">
-            <h1 id="text" style={{color: props.color}}><i className="fas fa-quote-left" style={{color: props.color}} /> {props.quote}</h1>
+            <h1 id="text" style={{color: props.color, transition: "1s"}}><i className="fas fa-quote-left" style={{color: props.color, transition: "1s"}} /> {props.quote}</h1>
             <p id="author">- {props.author}</p>
             <div id="buttons">
-                <a id="tweet-quote" href={'https://twitter.com/intent/tweet?text="' + encodeURIComponent(props.quote) + '" - ' + encodeURIComponent(props.author)} className="button" style={{backgroundColor: props.color}}><i className="fab fa-twitter fa-lg"></i></a>
-                <button id="new-quote" onClick={props.randomQuote} className="button" style={{backgroundColor: props.color}} >New quote</button>
+                <a id="tweet-quote" target="_blank" href={'https://twitter.com/intent/tweet?text="' + encodeURIComponent(props.quote) + '" - ' + encodeURIComponent(props.author)} className="button" style={{backgroundColor: props.color, transition: "background-color 1s, transform .2s"}}><i className="fab fa-twitter fa-lg"></i></a>
+                <button id="new-quote" onClick={props.randomQuote} className="button" style={{backgroundColor: props.color, transition: "background-color 1s, transform .2s"}} >New quote</button>
             </div>
 
         </div>

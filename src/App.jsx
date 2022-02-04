778e813912a6973f33fbd95093d386a6568f3ea2
jero237 <jeronimobalestra@gmail.com>
2022-02-04 19:35:41 -0300

    Added css animations and improved responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ class App extends React.Component{
 
   render() {
     return (
-      <div className="App" style={{backgroundColor: this.state.color, transition: "1s"}}>
+      <div className="App" style={{backgroundColor: this.state.color, transition: "background-color 1s"}}>
         <QuoteBox quote={this.state.quote} author={this.state.author} color={this.state.color} randomQuote={this.randomQuote}/>
-        <a href="https://github.com/jero237" id='github' target="_blank"><i class="fab fa-github"></i> jero237</a>
+        <a href="https://github.com/jero237" id='github' target="_blank">Made with React.js<br /><i class="fab fa-github"></i> jero237</a>
       </div>
     );
   }
